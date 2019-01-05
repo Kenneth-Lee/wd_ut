f61d784a59d6f1aef3f342c513819dc09c284333
Kenneth Lee <liguozhu@hisilicon.com>
2019-01-05 11:26:18 +0800

    uacce: add more test case
    Signed-off-by: Kenneth Lee <liguozhu@hisilicon.com>

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 TARGET_DIR=~/work/hisi-repo/linux-kernel-warpdrive
 TESTED_DIR=$(TARGET_DIR)/drivers/uacce
-CFLAGS=-O0 --coverage -g -I general_stub -I $(TESTED_DIR) 
+CFLAGS=-O0 --static --coverage -g -I general_stub -I $(TESTED_DIR) 
 COMM_FILES=ut.c comm.h
 
 all: uacce.ut
@@ -9,15 +9,19 @@ uacce.ut: uacce.ut.c $(TESTED_DIR)/uacce.c $(COMM_FILES) prepare
 	$(CC) $(CFLAGS) $< -o $@
 
 test: all
-	@./uacce.ut 2> ut.log
+	@(./uacce.ut 2> ut.log)
 	@!(sed '/^[^0]/d' ut.log |addr2line -e uacce.ut |grep -v "ut/ut.c")
 
 clean:
-	@rm -f *.ut *.o *.log *.out core *.gcda *.gcno lcov.log
-	@rm -rf out
+	@rm -f *.ut *.o *.log *.out core *.gcda *.gcno lcov.log ut.info
+	@rm -rf testreport
 
 prepare:
 	ln -sf $(TARGET_DIR)/include/linux/uacce.h general_stub/linux/uacce.h
 	ln -sf $(TARGET_DIR)/include/uapi/linux/uacce.h general_stub/uapi/linux/uacce.h
 
-.PHONY: clean test all prepare
+report:
+	lcov -c -d . -o ut.info
+	genhtml ut.info -o testreport
+
+.PHONY: clean test all prepare report
--- uacce.ut.c
+++ uacce.ut.c
@@ -63,7 +63,6 @@ struct vm_operations_struct {
 #define get_order(size) 0
 #define remap_pfn_range(vma, addr, pfn, size, prot) 0
 #define page_to_pfn(page) 0
-#define vma_pages(vma) 0
 #define vunmap(kaddr)
 #define fget(fd) NULL
 #define vmap(pages, nr_pages, f1, f2) NULL
@@ -89,8 +88,10 @@ struct vm_operations_struct {
 #define	iommu_detach_device(domain, pdev)
 #define iommu_capable(bus, a) 0
 #define iommu_domain_free(domain)
-#define alloc_chrdev_region(devt, a, b, name) 0
+#define alloc_chrdev_region(...) 0
 #define	unregister_chrdev_region(devt, b)
+#define WARN_ON(...)
+#define sprintf(...) 0
 
 #define false 0
 #define true 1
@@ -133,6 +134,9 @@ struct iommu_domain {};
 struct iommu_domain _iommu_domain;
 struct iommu_domain *iommu_get_domain_for_dev(struct device *dev)
 {
+	if (testcase==400)
+		return NULL;
+
 	return &_iommu_domain;
 }
 
@@ -218,6 +222,11 @@ void __free_pages(struct page *page, unsigned int order)
 	case202_free++;
 }
 
+static int vma_pages_ret = 0;
+int vma_pages(struct vm_area_struct *vma) {
+	return vma_pages_ret;
+}
+
 #include "uacce.c"
 
 void case_iommu_map_qfr(void)
@@ -237,6 +246,9 @@ void case_iommu_map_qfr(void)
 	ut_assert(!ret);
 	ut_check_cnt_var_range(100, 110, get_page, 10);
 	ut_check_cnt_var_range(100, 110, iommu_map, 10);
+	uacce_iommu_unmap_qfr(&q, &qfr);
+	ut_check_cnt_var_range(100, 110, get_page, 0);
+	ut_check_cnt_var_range(100, 110, iommu_map, 0);
 
 	/* fail in the middle */
 	testcase = 101;
@@ -296,9 +308,166 @@ void case_qfr_alloc_pages(void)
 	/* todo: more test to fail case */
 }
 
+void case_qfr_string(void) {
+	struct uacce_qfile_region qfr;
+	qfr.type = 10; /* use an error type to see if it is crash */
+	uacce_qfrt_str(&qfr);
+}
+
+void case_map_qfr(void) {
+	struct uacce uacce;
+	struct uacce_ops ops;
+	struct uacce_queue q;
+	struct uacce_qfile_region qfr;
+	struct page *pages[10];
+	int ret;
+
+	q.uacce = &uacce;
+	uacce.ops = &ops;
+	qfr.pages = pages;
+	qfr.nr_pages = 10;
+
+	/* no map req */
+	ret = uacce_queue_map_qfr(&q, &qfr);
+	ut_assert(!ret);
+	uacce_queue_unmap_qfr(&q, &qfr);
+
+	qfr.flags = UACCE_QFRF_MAP;
+	ret = uacce_queue_map_qfr(&q, &qfr);
+	ut_assert(ret);
+	uacce_queue_unmap_qfr(&q, &qfr);
+}
+
+void case_create_region(void) {
+	struct uacce_queue q;
+	struct uacce_qfile_region *qfr;
+	struct vm_area_struct vma;
+	int ret;
+
+	qfr = uacce_create_region(&q, &vma, 0, 0);
+	ut_assert(qfr);
+	uacce_destroy_region(qfr);
+}
+
+int case_start_queue_check = 0;
+int start_queue_case_start_queue(struct uacce_queue *q) {
+	if (testcase == 601) {
+		return -1;
+	}
+
+	case_start_queue_check++;
+	return 0;
+}
+
+void case_start_queue(void) {
+	struct uacce_queue q;
+	struct uacce uacce;
+	struct uacce_ops ops;
+	int ret, i;
+
+	q.uacce = &uacce;
+	uacce.ops = &ops;
+	ops.start_queue = start_queue_case_start_queue;
+
+	for (i=0; i<UACCE_QFRT_MAX; i++)
+		q.qfrs[i] = NULL;
+
+	ret = uacce_start_queue(&q);
+	ut_assert(case_start_queue_check==1);
+	ut_assert(!ret);
+
+	testcase = 601;
+	ret = uacce_start_queue(&q);
+	ut_assert(ret);
+}
+
+void case_get_region_type(void) {
+	enum uacce_qfrt type;
+	struct uacce uacce;
+	struct uacce_ops ops;
+	struct vm_area_struct vma;
+	int i;
+
+#define CLEAR_QFRS do { \
+	for (i=0; i<UACCE_QFRT_MAX; i++) \
+		ops.qf_pg_start[i] = UACCE_QFR_NA; \
+	ops.flags = 0; \
+}while(0)
+
+	uacce.ops = &ops;
+
+	vma.vm_pgoff = 10; /* 10 page as start */
+	vma_pages_ret = 20; /* size is 20 page */
+
+	/* All invalid */
+	CLEAR_QFRS;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert(type == UACCE_QFRT_INVALID);
+
+	/* only ss, not in range */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 30;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert(type == UACCE_QFRT_INVALID);
+
+	/* only ss, in range */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 10;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert(type == UACCE_QFRT_SS);
+
+	/* only mmio ss, in mmio */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_MMIO] = 10;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 30;
+	ops.mmap = (int (*)(struct uacce_queue *q,
+		struct vm_area_struct *vma,
+		struct uacce_qfile_region *qfr))1;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert_str(type == UACCE_QFRT_MMIO, "type=%d", type);
+
+	/* only mmio ss, in mmio, bad size */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_MMIO] = 10;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 40;
+	ops.mmap = (int (*)(struct uacce_queue *q,
+		struct vm_area_struct *vma,
+		struct uacce_qfile_region *qfr))1;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert_str(type == UACCE_QFRT_INVALID, "type=%d", type);
+
+	/* only dko ss, in dko, good size */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_DKO] = 10;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 30;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert_str(type == UACCE_QFRT_DKO, "type=%d", type);
+
+	/* only dko ss, in dko, bad size */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_DKO] = 10;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 40;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert_str(type == UACCE_QFRT_INVALID, "type=%d", type);
+
+	/* have all, in dus, good size */
+	CLEAR_QFRS;
+	ops.qf_pg_start[UACCE_QFRT_MMIO] = 0;
+	ops.qf_pg_start[UACCE_QFRT_DKO] = 2;
+	ops.qf_pg_start[UACCE_QFRT_DUS] = 10;
+	ops.qf_pg_start[UACCE_QFRT_SS] = 30;
+	type = uacce_get_region_type(&uacce, &vma);
+	ut_assert_str(type == UACCE_QFRT_DUS, "type=%d", type);
+}
+
 int main(void)
 {
 	test(100, case_iommu_map_qfr);
 	test(200, case_qfr_alloc_pages);
+	test(300, case_qfr_string);
+	test(400, case_map_qfr);
+	test(500, case_create_region);
+	test(600, case_start_queue);
+	test(700, case_get_region_type);
 	return 0;
 }
